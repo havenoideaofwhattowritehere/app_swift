55a40ceab935e30005ad623b8da00c6d0dd59df3
Nikita Grinyov <94113099+havenoideaofwhattowritehere@users.noreply.github.com>
2024-10-31 12:26:01 +0200

    Update Chat.swift

---
+++ chat-app/backend/Sources/App/Models/Chat.swift
@@ -0,0 +1,19 @@
+import Fluent
+import Vapor
+
+final class Chat: Model, Content {
+    static let schema = "chats"
+
+    @ID(key: .id)
+    var id: UUID?
+
+    @Field(key: "title")
+    var title: String
+
+    init() { }
+
+    init(id: UUID? = nil, title: String) {
+        self.id = id
+        self.title = title
+    }
+}
